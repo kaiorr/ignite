f9172a6c05d5976ab5657b2f00f1918537637a5c
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-03-22 19:11:55 -0300

    feat: validation category already exists

--- trilha-backend/ChapterII/project-2/rentalx/src/routes/categorie.route.ts
+++ trilha-backend/ChapterII/project-2/rentalx/src/routes/categorie.route.ts
@@ -7,6 +7,12 @@ const categoriesRepository = new CategoryRepository()
 categoriesRouter.post('/', (req, res) => {
   const {name, description} = req.body
 
+  const categoryAlReadyExists = categoriesRepository.findByName(name)
+
+  if(categoryAlReadyExists) {
+    return res.status(400).json({error: "Category already exists"})
+  }
+
   categoriesRepository.create({name, description})
 
   return res.status(201).send()
