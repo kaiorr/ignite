66b8ad7a6bd6ffa9d7c8b8d94c6e35f11b7deeb2
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-05-26 20:19:10 -0300

    Adicionando especificaçoes

--- trilha-backend/ChapterII/project-2/rentalx/services/CreateCategoryService.ts
+++ trilha-backend/ChapterII/project-2/rentalx/services/CreateCategoryService.ts
@@ -1,23 +1,22 @@
-import { ICategoriesRepository } from '../repositories/ICategoriesRepository'
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
 
 interface IRequest {
-  name: string,
-  description: string
+  name: string;
+  description: string;
 }
 
 class CreateCategoryService {
+  // Dependency Inversion Principle
+  constructor(private categoryRepository: ICategoriesRepository) {}
 
-  //Dependency Inversion Principle
-  constructor (private categoryRepository: ICategoriesRepository) {  }
+  execute({ name, description }: IRequest): void {
+    const categoryAlReadyExists = this.categoryRepository.findByName(name);
 
-    execute ({name, description}: IRequest): void {
-      const categoryAlReadyExists = this.categoryRepository.findByName(name)
-
-      if(categoryAlReadyExists) {
-        throw new Error("Category already exists")
-      }
-      this.categoryRepository.create({name, description})
+    if (categoryAlReadyExists) {
+      throw new Error("Category already exists");
+    }
+    this.categoryRepository.create({ name, description });
   }
 }
 
-export { CreateCategoryService }
+export { CreateCategoryService };
