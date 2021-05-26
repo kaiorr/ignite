315b551e574d979d2230433f6cb298e2cf4e1524
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-05-26 20:20:14 -0300

    Adicioando configuraçoes

--- trilha-frontend/1/src/App.jsx
+++ trilha-frontend/1/src/App.jsx
@@ -1,5 +1,6 @@
 import './styles/global.scss';
+import { RepositoryList } from './components/RepositoryList';
 
 export function App() {
-  return (<h1>HIj</h1>)
+  return <RepositoryList />
 }
---
+++ trilha-frontend/1/src/components/RepositoryList.jsx
@@ -0,0 +1,54 @@
+export function RepositoryList() {
+  return (
+    <section className="reopsitory-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Form in React</p>
+
+          <a heref="">
+            Acessar Repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Form in React</p>
+
+          <a heref="">
+            Acessar Repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Form in React</p>
+
+          <a heref="">
+            Acessar Repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Form in React</p>
+
+          <a heref="">
+            Acessar Repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Form in React</p>
+
+          <a heref="">
+            Acessar Repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
